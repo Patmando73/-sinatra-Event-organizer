83b82542928eb459d029458dacfdc28e562f2c71
Patrick Carlton <pcarlton73@ggmail.com>
2015-06-25 14:33:03 -0500

    Add places class to models

--- controllers/places.rb
+++ controllers/places.rb
@@ -2,11 +2,11 @@ get "/new-place-form" do
   erb :"places/new-place-form"
 end
 
-get "/users/save-user" do
+get "/users/save-palce" do
 
   @new_place = Place.add({"name" => params["name"]})
 
-  erb :"users/save-user"
+  erb :"users/save-palce"
 end
 
 
---
+++ models/place.rb
@@ -0,0 +1,14 @@
+require_relative "../database_class_methods.rb"
+require_relative "../database_instance_methods.rb"
+
+class Place
+  extend DatabaseClassMethods
+  include DatabaseInstanceMethods
+  attr_accessor :id
+  attr_accessor :name
+
+  def initialize(options={})
+    @id = options["id"]
+    @name = options["name"]
+  end
+end
